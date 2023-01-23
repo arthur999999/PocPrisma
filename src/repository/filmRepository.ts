139fde4aa21dcba8d891e4eca7ee48ab28eeb9d5
arthur999999 <arthurhono.contato@gmail.com>
2023-01-23 16:25:10 -0300

    fix bug route get films

--- src/repository/filmRepository.ts
+++ src/repository/filmRepository.ts
@@ -27,7 +27,7 @@ async function deleteFilm(id: string) {
 }
 
 async function getFilms(): Promise<QueryResult<Film>>{
-    return connection.query(`SELECT *, categories.name as "categoryName" FROM films JOIN categories ON films."categoryId" = categories.id ;`)
+    return connection.query(`SELECT films.id, films.name, films.conclued, films.image, films."categoryId", categories.name as "categoryName" FROM films JOIN categories ON films."categoryId" = categories.id ;`)
 }
 
 async function getFilmsCategory(id: number): Promise<QueryResult<Film>>{
